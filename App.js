48607d200af321d5bee7e81f1b5e4f8dab0d8e1a
Eduardo <eduardooffside@gmail.com>
2021-06-06 01:28:52 -0300

    first app ReactNative

---
+++ App.js
@@ -0,0 +1,20 @@
+//import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text> Meu primeiro aplicativo Mobile </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- README.md
+++
@@ -1,2 +0,0 @@
-# stopwatch
-Stopwatch with ReactNative
